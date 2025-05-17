e52bb44eaba31abfce622e45077bfbfee632f9bd
Onni06 <onni.suominen@gmail.com>
2025-05-17 08:48:34 +0300

    dd

--- webapp/vite.config.js
+++ webapp/vite.config.js
@@ -4,5 +4,15 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: '/'
+  base: '/Onni06.github.io/',
+  build: {
+    outDir: 'dist',
+    assetsDir: 'assets',
+    sourcemap: true,
+    rollupOptions: {
+      output: {
+        manualChunks: undefined
+      }
+    }
+  }
 })
